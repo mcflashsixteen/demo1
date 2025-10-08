40f72c0115f5a0e7cf02ef700c6bd32926c30a51
Morgan Clark <144955665+mcflashsixteen@users.noreply.github.com>
2025-10-08 14:00:37 -0500

    Added Hello World

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("World Hello");
+=======
+        System.out.println("Hello World");
+>>>>>>> 1d0ddf5 (Added Hello World)
 
     }
 }
